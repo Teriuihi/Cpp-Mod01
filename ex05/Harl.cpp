8c7e3d1c0e9eb2fae1b947593f8fc628c36bb833
Stijn Appunn <sappunn@student.codam.nl>
2022-07-15 20:56:24 +0200

    created functions

---
+++ ex05/Harl.cpp
@@ -0,0 +1,21 @@
+#include "Harl.h"
+
+void Harl::complain(std::string level) {
+
+}
+
+void Harl::debug( void ) {
+
+}
+
+void Harl::info( void ) {
+
+}
+
+void Harl::warning( void ) {
+
+}
+
+void Harl::error( void ) {
+
+}
